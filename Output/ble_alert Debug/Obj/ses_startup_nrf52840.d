7040e4e2092dd02bd6abc71aaaa276a2cfe2f8b3
Michael Dougherty <31718646+SerenityRising@users.noreply.github.com>
2020-04-14 17:07:23 -0400

    removing junk files

--- Output/ble_alert Debug/Obj/ses_startup_nrf52840.d
+++
@@ -1,2 +0,0 @@
-Output/ble_alert\ Debug/Obj/ses_startup_nrf52840.o: \
- C:\Users\micha\OneDrive\Documents\Patrick\ SmartBarn\BLE_Projects\nRF5SDK_Current\modules\nrfx\mdk\ses_startup_nrf52840.s
--- Output/ble_alert Debug/Obj/thumb_crt0.d
+++
@@ -1,2 +0,0 @@
-Output/ble_alert\ Debug/Obj/thumb_crt0.o: \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.40/source/thumb_crt0.s
